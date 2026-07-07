8a7d45cb22714d6064fd5c24d94dd70134605477
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2026-07-07 21:09:03 +0000

    changed the parsing logic

--- src/bin/tcplistener.rs
+++ src/bin/tcplistener.rs
@@ -1,44 +1,10 @@
-use std::io::{self, Read};
 use std::net::TcpListener;
-use std::sync::mpsc::{self, Receiver};
-use std::thread;
 
-fn get_lines_channel( mut reader: impl Read + Send + 'static) -> Receiver<String> {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        let mut buffer = [0u8; 8];
-        let mut current_line = String::new();
-        loop {
-            let bytes_read = match reader.read(&mut buffer) {
-                Ok(0) => break,
-                Ok(n) => n,
-                Err(_) => break,
-            };
-            let chunk = String::from_utf8_lossy(&buffer[..bytes_read]);
-            let parts: Vec<&str> = chunk.split('\n').collect();
-            for part in &parts[..parts.len() - 1] {
-                let line = format!("{}{}", current_line, part);
+use httpfromsratch::internal::request::request_from_reader;
 
-                if tx.send(line).is_err() {
-                    return;
-                }
-
-                current_line.clear();
-            }
-            current_line.push_str(parts[parts.len() - 1]);
-        }
-        if !current_line.is_empty() {
-            let _ = tx.send(current_line);
-        }
-        // reader is closed automatically here when dropped
-        // tx is closed automatically here when dropped
-    });
-    rx
-}
-
-
-fn main() -> io::Result<()> {
+fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:42069")?;
+
     println!("Listening on 127.0.0.1:42069");
 
     for stream in listener.incoming() {
@@ -46,10 +12,19 @@ fn main() -> io::Result<()> {
 
         println!("Connection accepted");
 
-        let lines = get_lines_channel(stream);
+        match request_from_reader(stream) {
+            Ok(request) => {
+                let line = request.request_line.unwrap();
 
-        for line in lines {
-            println!("{}", line);
+                println!("Request line:");
+                println!("- Method: {}", line.method);
+                println!("- Target: {}", line.request_target);
+                println!("- Version: {}", line.http_version);
+            }
+
+            Err(e) => {
+                eprintln!("Error parsing request: {}", e);
+            }
         }
 
         println!("Connection closed");
